24ac506dd8e5d7402c0f0eeaabeabf043fc143e0
CarlosDevBr <cahzerohenrique@gmail.com>
2022-04-06 17:32:45 -0300

    Criado uma rota post para parkingSpot

--- src/main/java/com/api/packingcontrol/controllers/ParkingSpotController.java
+++ src/main/java/com/api/packingcontrol/controllers/ParkingSpotController.java
@@ -1,9 +1,16 @@
 package com.api.packingcontrol.controllers;
 
+import com.api.packingcontrol.dtos.ParkingSpotDto;
+import com.api.packingcontrol.models.ParkingSpotModel;
 import com.api.packingcontrol.services.ParkingSpotService;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -16,5 +23,11 @@ public class ParkingSpotController {
         this.parkingSpotService = parkingSpotService;
     }
 
-
+    @PostMapping
+    public ResponseEntity<Object> saveParkingSpot(@RequestBody @Valid ParkingSpotDto parkingSpotDto){
+        var parkingSpotModel = new ParkingSpotModel();
+        BeanUtils.copyProperties(parkingSpotDto, parkingSpotModel);
+        parkingSpotModel.setRegistrationDate(LocalDateTime.now(ZoneId.of("UTC")));
+        return ResponseEntity.status(HttpStatus.CREATED).body(parkingSpotService.save(parkingSpotModel));
+    }
 }
