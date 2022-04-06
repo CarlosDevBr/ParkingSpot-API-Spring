bdbc39164322cd123bbbb380fa0a800282901b27
CarlosDevBr <cahzerohenrique@gmail.com>
2022-04-06 17:19:09 -0300

    Criado controller de parkingSpot

---
+++ src/main/java/com/api/packingcontrol/controllers/ParkingSpotController.java
@@ -0,0 +1,20 @@
+package com.api.packingcontrol.controllers;
+
+import com.api.packingcontrol.services.ParkingSpotService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping("/parking-spot")
+public class ParkingSpotController {
+
+    final ParkingSpotService parkingSpotService;
+
+    public ParkingSpotController(ParkingSpotService parkingSpotService) {
+        this.parkingSpotService = parkingSpotService;
+    }
+
+    
+}
