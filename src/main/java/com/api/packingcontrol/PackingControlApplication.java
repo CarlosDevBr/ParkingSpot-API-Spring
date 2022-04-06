67f4ae287fa0e251449e92b99337cc6bfba38419
CarlosDevBr <cahzerohenrique@gmail.com>
2022-04-06 16:40:36 -0300

    Conectando ao banco de dados

---
+++ src/main/java/com/api/packingcontrol/PackingControlApplication.java
@@ -0,0 +1,13 @@
+package com.api.packingcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackingControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PackingControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/packingcontrol/PackingControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.packingcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackingControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
