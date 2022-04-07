ee2f8bdf940584efb867166290051554dcb27c56
CarlosDevBr <cahzerohenrique@gmail.com>
2022-04-07 15:45:54 -0300

    Criado os metodos de validação no repository e service

--- src/main/java/com/api/packingcontrol/repositories/ParkingSpotRepository.java
+++ src/main/java/com/api/packingcontrol/repositories/ParkingSpotRepository.java
@@ -9,4 +9,7 @@ import java.util.UUID;
 @Repository
 public interface ParkingSpotRepository extends JpaRepository<ParkingSpotModel, UUID> {
 
+    public boolean existsByLicensePlateCar(String licensePlateCar);
+    public boolean existsByParkingSpotNumber(String parkingSpotNumber);
+    public boolean existsByApartmentAndBlock(String apartment, String block);
 }
